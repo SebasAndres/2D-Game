dd7fe1f7cc0c50b4922311fbc4f34ca58abaef7c
Sebastián Andrés <69599597+SebasAndres@users.noreply.github.com>
2022-01-24 15:00:14 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
-int main() {
-	
-	// Mirar el codigo en Source Files y Header files
-	
+int main() {	
+	// Mirar el codigo en Source Files y Header files	
+	// Mirar el codigo en Source Files y Header files	
+	// Mirar el codigo en Source Files y Header files	
 	Game nGame = Game();
 	while (nGame.isRunning()) {
 		nGame.update(); nGame.render();	
